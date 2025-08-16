a7fc28c653b6ed4147e128e4334101f4f9998a55
Jeremy0139 <72512276+Jeremy0139@users.noreply.github.com>
2025-08-16 11:25:37 +0800

    Update food_ordering.asm

--- food_ordering.asm
+++ food_ordering.asm
@@ -10,7 +10,7 @@ charxiufan_name db "4.Char Xiu Fan $",
 wan_tan_mee_name db "5.Wan Tan Mee $",
 chicken_rice_price db " = RM 6.50 $",
 egg_price db " = RM 1.50 $",
-roasted_pork_price db " = RM 1888 $",
+roasted_pork_price db " = RM 188.00 $",
 charxiufan_price db " = RM 11.00 $",
 wan_tan_mee_price db " = RM 7.50 $",
 msg7 db "Please choose your meals (1~5) > $",
@@ -23,11 +23,26 @@ wan_tan_mee_msg db "Wan Tan Mee $",
 current_price db "your current price is : RM $",
 price db ?
 qty db ? ; to store user input qty
+
+price_chicken db ' 6.50$',
+price_egg db ' 1.50$',
+price_roasted_pork db ' 188.00$',
+price_charxiufan db '11.00$',
+price_wan_tan_mee db '7.50$',
+
 .code 
 main PROC
     mov ax, @data
     mov ds,ax
 
+    ;clear screen
+    mov ax, 0600h
+    mov bh,71h
+    mov cx,0h
+    mov dx, 184fh
+    int 10h
+    
+    ;program start
     mov ah, 09h ; Welcome to food ordering system
     lea dx, msg 
     int 21h 
@@ -204,6 +219,60 @@ print_current_price:
 
     mov ah, 09h ; current price 
     lea dx, current_price 
+    int 21h
+
+    mov al,30h
+    mov cx, 1
+
+    print:
+
+    cmp qty, '1' ; chicken rice 
+    je display1
+    cmp qty, '2' ; egg
+    je display2
+    cmp qty, '3' ; Roasted Pork
+    je display3
+    cmp qty, '4' ; Char Xiu Fan 
+    je display4
+    cmp qty, '5' ; wan tan mee
+    je display5
+
+    jmp exit_program 
+    continue:
+
+    loop print 
+    jmp exit_program
+display1:
+    mov ah,09h
+    lea dx, price_chicken
+    int 21h
+
+    jmp continue
+
+ display2:
+    mov ah,09h
+    lea dx, price_egg
+    int 21h
+
+    jmp continue
+
+display3:
+    mov ah,09h
+    lea dx, price_roasted_pork
+    int 21h
+
+    jmp continue
+
+display4:
+    mov ah,09h
+    lea dx, price_charxiufan
+    int 21h
+
+    jmp continue
+
+display5:
+    mov ah,09h
+    lea dx, price_wan_tan_mee
     int 21h
 
 exit_program:
